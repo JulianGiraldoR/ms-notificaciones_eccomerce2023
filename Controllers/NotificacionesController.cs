0c3afc55143a231117d720ac2a3372d934c3a6b4
JulianGiraldoR <juliangato87@gmail.com>
2023-04-27 21:23:54 -0500

    creacion de modulos de recuperacion de contraseña y bienvenida

--- Controllers/NotificacionesController.cs
+++ Controllers/NotificacionesController.cs
@@ -3,31 +3,101 @@ using SendGrid;
 using SendGrid.Helpers.Mail;
 using ms_notificaciones.Models;
 
+
+
 namespace ms_notificaciones.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class NotificacionesController : ControllerBase
 {
-    [Route("correo")]
-    public async Task<ActionResult> EnvviarCorreo(ModeloCorreo datos){
+    [Route("correo-bienvenida")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreoBienvenida(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
 
-            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-            
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("jgiral28@ibero.edu.co", "Julian Andres Giraldo");
-            var subject = datos.asuntoCorreo;
-            var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
-            var plainTextContent = "plain text content";
-            var htmlContent = datos.contenidoCorreo;
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-            if(response.StatusCode == System.Net.HttpStatusCode.Accepted){
-                return Ok("Correo enviado a la direccion" + datos.correoDestino);
-            }else{
-                return BadRequest("Error enviando el mensaje a la direccion" + datos.correoDestino);
-            }
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name = datos.nombreDestino,
+            message = "Bienvenido a ventas y servicios axm."
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la dirección " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+        }
+    }
 
 
+    [Route("correo-recuperacion-clave")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreoRecuperacionClave(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name = datos.nombreDestino,
+            message = "Esta es su nuevla clave... no la comparta."
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la dirección " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+        }
+    }
+
+
+    [Route("enviar-correo-2fa")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreo2fa(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("TwoFA_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            nombre = datos.nombreDestino,
+            mensaje = datos.contenidoCorreo,
+            asunto = datos.asuntoCorreo
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la dirección " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+        }
+    }
+
+
+    private SendGridMessage CrearMensajeBase(ModeloCorreo datos)
+    {
+        var from = new EmailAddress(Environment.GetEnvironmentVariable("EMAIL_FROM"), Environment.GetEnvironmentVariable("NAME_FROM"));
+        var subject = datos.asuntoCorreo;
+        var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
+        var plainTextContent = datos.contenidoCorreo;
+        var htmlContent = datos.contenidoCorreo;
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        return msg;
     }
 }
